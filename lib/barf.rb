cf3de91d971a21c9c1e7511b85858765b9c26af0
Jacob Duffy <duffy.jp@gmail.com>
2018-12-27 13:56:26 -0600

    Extracted the tco palette and applied to source image with FloydSteinberg dithering. Tco will still best-match each pixel, but they'll be perfect matches unless you have a 16 color terminal.

--- lib/barf.rb
+++ lib/barf.rb
@@ -5,15 +5,21 @@ require "tco"
 
 module Barf
 
+  @palette = "inline:data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAP4AAAABCAIAAABxMITKAAABpklEQVR4AW2StWIUXRTHf2d2o9+Huzv85yGooKJKic7wKukueYvZICXW0kZ67CwV7lbh7CVzd+IZ/dv1Y8Dw8PDo8uvYzHSnsIJ4oyrPQafk1KtXfWtoaKjVarXb7cHBwTm6aWJiM2wHwQZjVySUCEtPL1SliDU2QoFoHlPFARihdENWqWcZRc+qWwWy0iOiGqusW8YTlFR4iaLC5ObNbP8cXWkIjyJz9UQW6m8zbrh1SZAyyFnAYayqsRfCGpdq/Rt27mo/8YsiujhO3JjlQ1FG6wwPQVXpg+gcDrpXaQidxMl095KPoNNJVyeI6JarR5vW0eyhIh6VVu0CNykiEkaC0JGiOScEosmst9e72R0uLTbJ8RPg1DjLA2mWJvjeLp49sESjmEx7YQlf9kXcCfV3o+urKLtO3Zdo8qmrDa9t1+5LvrhNh8bH+0fcP+5N7965Un+ufuTQz58LgbnvztlZRTxHsJARFvQ44TRHl4zuCU6A3JNe465rAV8JDV7eT8qvpYfcFdMql+s7ZmYWqvTY37/NQF0p4mLdq1eZZcMjw5s2bfoHC6PO/+pdxgEAAAAASUVORK5CYII="
+
   def self.print(path)
     image = MiniMagick::Image.open(path)
 
     terminal_width = `tput cols`.to_i
 
     # Height must be divisible by 2 for the half pixels to be clean.
-    new_height     = (image.height * (0.5 * terminal_width / image.width)).to_i * 2
+    new_height     = [2, (image.height * (0.5 * terminal_width / image.width)).to_i * 2].max
 
-    image.resize "#{terminal_width}x#{new_height}!"
+    image.combine_options do |tmp|
+      tmp.resize "#{terminal_width}x#{new_height}!"
+      tmp.dither 'FloydSteinberg'
+      tmp.remap @palette
+    end
 
     # Two dimensional array of pixels:
     image.get_pixels.each_slice(2) do |top, bottom|
