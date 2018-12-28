3f100fd5704a00ecceac2342cf0008b897def448
Jacob Duffy <duffy.jp@gmail.com>
2018-12-28 14:10:29 -0600

    Imagemagick 6.9 and 7.0 can't load the palette inline (bug), so I'm loading from a file instead.

--- lib/barf.rb
+++ lib/barf.rb
@@ -6,8 +6,6 @@ require "parallel"
 
 module Barf
 
-  @palette = "inline:data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAP4AAAABCAIAAABxMITKAAABpklEQVR4AW2StWIUXRTHf2d2o9+Huzv85yGooKJKic7wKukueYvZICXW0kZ67CwV7lbh7CVzd+IZ/dv1Y8Dw8PDo8uvYzHSnsIJ4oyrPQafk1KtXfWtoaKjVarXb7cHBwTm6aWJiM2wHwQZjVySUCEtPL1SliDU2QoFoHlPFARihdENWqWcZRc+qWwWy0iOiGqusW8YTlFR4iaLC5ObNbP8cXWkIjyJz9UQW6m8zbrh1SZAyyFnAYayqsRfCGpdq/Rt27mo/8YsiujhO3JjlQ1FG6wwPQVXpg+gcDrpXaQidxMl095KPoNNJVyeI6JarR5vW0eyhIh6VVu0CNykiEkaC0JGiOScEosmst9e72R0uLTbJ8RPg1DjLA2mWJvjeLp49sESjmEx7YQlf9kXcCfV3o+urKLtO3Zdo8qmrDa9t1+5LvrhNh8bH+0fcP+5N7965Un+ufuTQz58LgbnvztlZRTxHsJARFvQ44TRHl4zuCU6A3JNe465rAV8JDV7eT8qvpYfcFdMql+s7ZmYWqvTY37/NQF0p4mLdq1eZZcMjw5s2bfoHC6PO/+pdxgEAAAAASUVORK5CYII="
-
   def self.print(path)
     image = MiniMagick::Image.open(path)
 
@@ -21,7 +19,7 @@ module Barf
       tmp.flatten
       tmp.resize "#{terminal_width}x#{new_height}!"
       tmp.dither 'FloydSteinberg'
-      tmp.remap @palette
+      tmp.remap __dir__ + '/palette.png'
     end
 
     # Two dimensional array of pixels:
